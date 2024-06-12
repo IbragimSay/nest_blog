08305463fc462343624df89bf6174196bff7aa43
IbragimSay <argen.muratov.2000@mail.ru>
2024-06-12 16:28:48 +0300

    feat: Добавил controller для post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { createPostDto, updataPostDto } from './dto';
+import { PostService } from './post.service';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+    constructor(
+        private readonly postService:PostService
+    ){}
+    
+    @Get(":id")
+    getOne(@Param("id") id:number ){
+        return this.postService.getOne(+id)
+    }
+
+    @Post()
+    create(@Body() dto:createPostDto){
+        return this.postService.save(dto)
+    }
+
+    @Patch(":id")
+    updata(@Param("id") id:number, @Body() dto: updataPostDto){
+        return this.postService.updata(+id, dto)
+    }
+
+    @Delete(":id")
+    async delete(@Param("id") id:number){
+        const post = await this.postService.getOne(+id)
+        if(!post){
+            throw new BadRequestException()
+        }
+        return this.postService.delete(+id)
+    }
+}
